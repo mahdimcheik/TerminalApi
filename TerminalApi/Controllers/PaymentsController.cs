cbbc92be98ec28eb59146d2fd2e16e16d8a5c0a7
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-02-20 23:54:49 +0100

    wip , paynment, merci chacha

---
+++ TerminalApi/Controllers/PaymentsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
+using Stripe;
+using TerminalApi.Utilities;
+
+namespace TerminalApi.Controllers
+{
+    [Route("api/payments")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        public PaymentsController()
+        {
+            StripeConfiguration.ApiKey = Environment.GetEnvironmentVariable("STRIPE_PUBLISHABLEKEY"); 
+        }
+
+        [HttpPost("create-checkout-session")]
+        public IActionResult CreateCheckoutSession([FromBody] CheckoutRequest request)
+        {
+            var domain = EnvironmentVariables.API_FRONT_URL; 
+
+            var options = new SessionCreateOptions
+            {
+                PaymentMethodTypes = new List<string> { "card" },
+                LineItems = new List<SessionLineItemOptions>
+            {
+                new()
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        Currency = "usd",
+                        UnitAmount = (long)(request.Amount * 100),
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = request.ItemName
+                        }
+                    },
+                    Quantity = 1
+                }
+            },
+                Mode = "payment",
+                SuccessUrl = $"{domain}/success",
+                CancelUrl = $"{domain}/cancel"
+            };
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            return Ok(new { sessionId = session.Id, url = session.Url });
+        }
+    }
+
+    public class CheckoutRequest
+    {
+        public string? ItemName { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
