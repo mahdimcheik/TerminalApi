619aa6890985c1841ec07d8e060e4658e6a352ae
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-03-05 23:41:40 +0100

    wip filters

--- TerminalApi/Models/QueryPagination.cs
+++ TerminalApi/Models/QueryPagination.cs
@@ -7,5 +7,7 @@
         public string? StudentId { get; set; }
         public DateTimeOffset? FromDate { get; set; }
         public DateTimeOffset? ToDate { get; set; }
+        public short? OrderByName { get; set; } = 0;
+        public short? OrderByDate { get; set; } = 0;
     }
 }
