3a74ee26af666ae0092f132e0d3a04de1c623e80
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-03-31 23:34:43 +0200

    fixture addresses + formations

---
+++ TerminalApi/Controllers/FixturesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using TerminalApi.Contexts;
+using TerminalApi.Models.Adresse;
+using TerminalApi.Models.Formations;
+using TerminalApi.Services;
+
+namespace TerminalApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FixturesController : ControllerBase
+    {
+        private readonly ApiDefaultContext context;
+        private readonly FakerService fakerService;
+        private readonly FormationService formationService;
+
+        public FixturesController(ApiDefaultContext context,FakerService fakerService ,FormationService formationService)
+        {
+            this.context = context;
+            this.fakerService = fakerService;
+            this.formationService = formationService;
+        }
+
+        [HttpGet("add-formations")]
+        public async Task<IActionResult> AddFormations()
+        {
+            var users =  context.Users.ToList();
+
+            foreach(var user in users)
+            {
+                int randomInt = new Random().Next(1, 5);
+                var formations = fakerService.GenerateFormationsCreateDTO().Generate(randomInt);
+                foreach (var formation in formations)
+                {
+                    context.Formations.Add(formation.ToFormation(user.Id));
+                }
+            }
+
+            await context.SaveChangesAsync();
+            return Ok("Formations ajoutées");
+        }
+
+        [HttpGet("add-addresses")]
+        public async Task<IActionResult> AddAddresses()
+        {
+            var users = context.Users.ToList();
+
+            foreach (var user in users)
+            {
+                int randomInt = new Random().Next(1, 3);
+                List<AddressCreateDTO>? addresses = fakerService.GenerateAddresseCreateDTO().Generate(randomInt);
+                foreach (var addresse in addresses)
+                {
+                    context.Addresses.Add(addresse.ToAddress(user.Id));
+                }
+            }
+
+            await context.SaveChangesAsync();
+            return Ok("Addresses ajoutées");
+        }
+    }
+}
--- TerminalApi/Services/FakerService.cs
+++ TerminalApi/Services/FakerService.cs
@@ -1,4 +1,8 @@
 ﻿using Bogus;
+using Bogus.Extensions;
+using System.ComponentModel.DataAnnotations;
+using TerminalApi.Models.Adresse;
+using TerminalApi.Models.Formations;
 using TerminalApi.Models.User;
 using TerminalApi.Utilities;
 
@@ -18,6 +22,28 @@ namespace TerminalApi.Services
                 .RuleFor(u => u.Title, f => f.Name.JobTitle()) // Optional job title
                 .RuleFor(u => u.Gender, f => f.PickRandom<EnumGender>()) // Pick a random EnumGender
                 .RuleFor(u => u.DateOfBirth, f => f.Date.Past(30, DateTime.Now.AddYears(-18)).ToUniversalTime()); // Between 18 and 30 years ago
+        }
+
+        public Faker<FormationCreateDTO> GenerateFormationsCreateDTO()
+        {
+            return new Faker<FormationCreateDTO>()
+                .RuleFor(u => u.City, f => f.Address.City())
+                .RuleFor(u => u.Company, f => f.Company.CompanyName())
+                .RuleFor(u => u.Country, f => f.Address.Country())
+                .RuleFor(u => u.EndAt, f => f.Date.Recent(0).ToUniversalTime())
+                .RuleFor(u => u.StartAt, f => f.Date.Recent(0).ToUniversalTime())
+                .RuleFor(u => u.Title, f => f.Name.JobTitle());
+        }
+
+        public Faker<AddressCreateDTO> GenerateAddresseCreateDTO()
+        {
+            return new Faker<AddressCreateDTO>()
+                .RuleFor(u => u.City, f => f.Address.City().ClampLength(max:49))                
+                .RuleFor(u => u.StreetLine2, f => f.Address.CardinalDirection())
+                .RuleFor(u => u.Country, f => f.Address.Country().ClampLength(max: 49))
+                .RuleFor(u => u.Street, f => f.Address.StreetName())
+                .RuleFor(u => u.PostalCode, f => f.Address.ZipCode().ClampLength(max: 19))
+                .RuleFor(u => u.AddressType, f => f.PickRandom<AddressTypeEnum>());
         }
     }
 }
