779b0f93f05c4a71873b6547bf22018ade63648b
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-04-11 11:47:39 +0200

    invoice model => takes order as input

--- TerminalApi/Controllers/BillController.cs
+++ TerminalApi/Controllers/BillController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using TerminalApi.Contexts;
+using TerminalApi.Models;
+using TerminalApi.Models.Payments;
 using TerminalApi.Services;
 
 namespace TerminalApi.Controllers
@@ -10,24 +15,45 @@ namespace TerminalApi.Controllers
     public class BillController : ControllerBase
     {
         private readonly PdfService pdfService;
+        private readonly ApiDefaultContext context;
 
-        public BillController(PdfService pdfService)
+        public BillController(PdfService pdfService, ApiDefaultContext context)
         {
             this.pdfService = pdfService;
+            this.context = context;
         }
 
         [HttpGet]
         public async Task<IActionResult> Get([FromQuery] string orderId)
         {
             Console.WriteLine(orderId);
-            await pdfService.GeneratePdfAsync(orderId);
-            return Ok();
-        }
-        [HttpGet("testing")]
-        public async Task<IActionResult> GetTest()
-        {
-            var file = await pdfService.GeneratePdfAsync("");
+            if (orderId.Trim().IsNullOrEmpty())
+            {
+                return BadRequest(
+                    new ResponseDTO { Message = "Aucune commande correspondante", Status = 404, }
+                );
+            }
+
+            var order = context
+                .Orders.Where(x => x.Id == Guid.Parse(orderId))
+                .Include(x => x.Booker)
+                .Include(x => x.Bookings)
+                .ThenInclude(x => x.Slot)
+                .FirstOrDefault();
+            if (order is null)
+            {
+                return BadRequest(
+                    new ResponseDTO { Message = "Aucune commande correspondante", Status = 404, }
+                );
+            }
+            var file = await pdfService.GeneratePdfAsync(order.ToOrderResponseForStudentDTO());
             return File(file, "application/pdf", "facture.pdf");
         }
+        //[HttpGet("testing")]
+        //public async Task<IActionResult> GetTest()
+        //{
+        //    //var file = await pdfService.GeneratePdfAsync("");
+        //    //return File(file, "application/pdf", "facture.pdf");
+        //}
     }
 }
--- TerminalApi/Services/PdfService.cs
+++ TerminalApi/Services/PdfService.cs
@@ -28,59 +28,15 @@ namespace TerminalApi.Services
                 .Build();
         }
 
-        public async Task<byte[]> GeneratePdfAsync(string orderId)
+        public async Task<byte[]> GeneratePdfAsync(OrderResponseForStudentDTO order)
         {
-            var res = Guid.TryParse(orderId, out Guid guidId);
-           
-            //var orderDetails = await context.Orders
-            //    .Where(x => x.Id == guid)
-            //    .Select(x => new OrderDetailsDto
-            //    {
-            //        BookerImgUrl = x.Booker.ImgUrl ?? "",
-            //        Bookings = x.Bookings.Select(b => new BookingDetailsDto
-            //        {
-            //            BookingCreatedAt = b.CreatedAt,
-            //            SlotStartAt = b.Slot.StartAt,
-            //            SlotEndAt = b.Slot.EndAt
-            //        }).ToList()
-            //    })
-            //    .FirstOrDefaultAsync();
-            //if (orderDetails is null) return;
-            // Render the HTML content using RazorLight
+            //var res = Guid.TryParse(orderId, out Guid guidId);
+
             string templatePath = "Invoice.cshtml"; // Name of your template file
 
-            var items = new List<InvoiceItem>
-            {
-                new InvoiceItem { Description = "Item 1", Quantity = 2, UnitPrice = 10 },
-                new InvoiceItem { Description = "Item 2", Quantity = 1, UnitPrice = 20 },
-                new InvoiceItem { Description = "Item 3", Quantity = 3, UnitPrice = 30 },
-                new InvoiceItem { Description = "Item 4", Quantity = 4, UnitPrice = 40 },
-                new InvoiceItem { Description = "Item 5", Quantity = 5, UnitPrice = 50 },
-                new InvoiceItem { Description = "Item 6", Quantity = 6, UnitPrice = 60 },
-                new InvoiceItem { Description = "Item 7", Quantity = 7, UnitPrice = 70 },
-                new InvoiceItem { Description = "Item 8", Quantity = 8, UnitPrice = 80 },
-                new InvoiceItem { Description = "Item 1", Quantity = 2, UnitPrice = 10 },
-                new InvoiceItem { Description = "Item 2", Quantity = 1, UnitPrice = 20 },
-                new InvoiceItem { Description = "Item 3", Quantity = 3, UnitPrice = 30 },
-                new InvoiceItem { Description = "Item 4", Quantity = 4, UnitPrice = 40 },
-                new InvoiceItem { Description = "Item 5", Quantity = 5, UnitPrice = 50 },
-                new InvoiceItem { Description = "Item 6", Quantity = 6, UnitPrice = 60 },
-                new InvoiceItem { Description = "Item 7", Quantity = 7, UnitPrice = 70 },
-                new InvoiceItem { Description = "Item 8", Quantity = 8, UnitPrice = 80 },
-            };
-            var model = new InvoiceModel
-            {
-                CompanyName = "Company Name",
-                CompanyEmail = "email@email.com",
-                InvoiceDate = DateTime.Now,
-                CustomerName = "Customer Name",
-                CustomerAddress = "Customer Address",
-                Items = items
-            };
-            string htmlContent = await _razorLightEngine.CompileRenderAsync(templatePath, model);
 
-            // Generate PDF using PuppeteerSharp
-            // await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+            string htmlContent = await _razorLightEngine.CompileRenderAsync(templatePath, order);
+
             using var browser = await Puppeteer.LaunchAsync(
                 new LaunchOptions { Headless = true }
             );
@@ -92,7 +48,7 @@ namespace TerminalApi.Services
                 Format = PaperFormat.A4,
                 DisplayHeaderFooter = true,
                 FooterTemplate = @"<div style='width:100%;text-align:center;font-size:10px;padding:5px;'>
-                Page <span class='pageNumber'></span> of <span class='totalPages'></span>
+                Page <span class='pageNumber'></span> / <span class='totalPages'></span>
             </div>",
                 MarginOptions = new PuppeteerSharp.Media.MarginOptions { Top = "40px", Bottom = "60px" }
             });
