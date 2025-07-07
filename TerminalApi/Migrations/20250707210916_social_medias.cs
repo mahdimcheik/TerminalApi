c4bc31af132f065c22722e3c948abb6495195f70
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-07-07 23:10:40 +0200

    add social medai fields

---
+++ TerminalApi/Migrations/20250707210916_social_medias.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TerminalApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class social_medias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TVARates",
+                keyColumn: "Id",
+                keyValue: new Guid("cd4fe9f4-2bd4-433c-8b45-3736663e5184"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surface",
+                table: "Layouts",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Preset",
+                table: "Layouts",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MenuMode",
+                table: "Layouts",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GithubUrl",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LinkedinUrl",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "TVARates",
+                columns: new[] { "Id", "Rate", "StartAt" },
+                values: new object[] { new Guid("967063ef-31ff-4162-8ee7-6c0fea4e92e2"), 0.2m, new DateTime(2025, 7, 7, 21, 9, 15, 496, DateTimeKind.Utc).AddTicks(5312) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TVARates",
+                keyColumn: "Id",
+                keyValue: new Guid("967063ef-31ff-4162-8ee7-6c0fea4e92e2"));
+
+            migrationBuilder.DropColumn(
+                name: "GithubUrl",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LinkedinUrl",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surface",
+                table: "Layouts",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Preset",
+                table: "Layouts",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MenuMode",
+                table: "Layouts",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "TVARates",
+                columns: new[] { "Id", "Rate", "StartAt" },
+                values: new object[] { new Guid("cd4fe9f4-2bd4-433c-8b45-3736663e5184"), 0.2m, new DateTime(2025, 5, 14, 18, 47, 23, 505, DateTimeKind.Utc).AddTicks(3454) });
+        }
+    }
+}
