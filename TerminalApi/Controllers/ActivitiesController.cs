c2832bc5623bc7720ab5578754ca4d377a504b35
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-07-09 15:35:01 +0200

    activiteis controller

--- TerminalApi/Controllers/ActivitiesController.cs
+++ TerminalApi/Controllers/ActivitiesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TerminalApi.Contexts;
@@ -8,6 +9,7 @@ namespace TerminalApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ActivitiesController : ControllerBase
     {
         private readonly ApiDefaultContext context;
@@ -17,8 +19,8 @@ namespace TerminalApi.Controllers
             this.context = context;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<List<Activities>> > GetTeacherTraffic()
+        [HttpGet("teacher")]
+        public async Task<ActionResult<ResponseDTO<ActivitiesTeacher>>> GetTeacherTraffic()
         {
          
             var firstDayOfWeek = DateTime.Now.Date.AddDays(-(int)DateTime.Now.DayOfWeek);
@@ -40,14 +42,48 @@ namespace TerminalApi.Controllers
                 .Select(u => u.ToUserResponseDTO(null))
                 .ToListAsync();
 
-            var trafficData = new Activities
+            var trafficData = new ActivitiesTeacher
             {
                 NewStudents = newStudents,
                 BookingsOftheWeek = bookinsOftheWeek,
                 OrdersOfTheWeek = OrdersOfTheWeek
             };
 
-            return Ok(new ResponseDTO<Activities>
+            return Ok(new ResponseDTO<ActivitiesTeacher>
+            {
+                Message = "Activités envoyées qvec succés",
+                Data = trafficData,
+                Status = 200
+            });
+
+        }
+
+
+        [HttpGet("student/{id}")]
+        public async Task<ActionResult<ResponseDTO<ActivitiesStudent>>> GetStudentTraffic([FromRoute] string id)
+        {
+
+            var firstDayOfWeek = DateTime.Now.Date.AddDays(-(int)DateTime.Now.DayOfWeek);
+
+            var bookinsOftheWeek = await context.Bookings
+                .Include(b => b.Slot)
+                .Include(b => b.Booker)
+                .Where(b => b.Slot.StartAt >= firstDayOfWeek && b.Slot.EndAt < firstDayOfWeek.AddDays(7) && b.BookedById == id)
+                .Select(b => b.ToBookingResponseDTO())
+                .ToListAsync();
+
+            var OrdersHistory = await context.Orders
+                .Where(o => o.UpdatedAt >= firstDayOfWeek && o.PaymentIntent != null && o.BookerId == id)
+                .Select(o => o.ToOrderResponseForTeacherDTO())
+                .ToListAsync();
+
+            var trafficData = new ActivitiesStudent
+            {
+                BookingsOftheWeek = bookinsOftheWeek,
+                OrdersHistory = OrdersHistory
+            };
+
+            return Ok(new ResponseDTO<ActivitiesStudent>
             {
                 Message = "Activités envoyées qvec succés",
                 Data = trafficData,
--- TerminalApi/Models/Activities/ActivitiesTeacher.cs
+++ TerminalApi/Models/Activities/ActivitiesTeacher.cs
@@ -1,9 +1,15 @@
 ﻿namespace TerminalApi.Models
 {
-    public class Activities
+    public class ActivitiesTeacher
     {
         public List<OrderResponseForTeacherDTO> OrdersOfTheWeek { get; set; } = new List<OrderResponseForTeacherDTO>();
         public List<BookingResponseDTO> BookingsOftheWeek { get; set; } = new List<BookingResponseDTO>();    
         public List<UserResponseDTO> NewStudents { get; set; } = new List<UserResponseDTO>();
+    }
+
+    public class ActivitiesStudent
+    {
+        public List<OrderResponseForTeacherDTO> OrdersHistory { get; set; } = new List<OrderResponseForTeacherDTO>();
+        public List<BookingResponseDTO> BookingsOftheWeek { get; set; } = new List<BookingResponseDTO>();
     }
 }
