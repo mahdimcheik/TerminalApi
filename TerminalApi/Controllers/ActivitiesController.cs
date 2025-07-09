d80620525b552aa02b17cea77e10eadaafb37c1e
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-07-09 10:49:21 +0200

    activities prof

---
+++ TerminalApi/Controllers/ActivitiesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TerminalApi.Contexts;
+using TerminalApi.Models;
+
+namespace TerminalApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly ApiDefaultContext context;
+
+        public ActivitiesController(ApiDefaultContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activities>> > GetTeacherTraffic()
+        {
+         
+            var firstDayOfWeek = DateTime.Now.Date.AddDays(-(int)DateTime.Now.DayOfWeek);
+
+            var bookinsOftheWeek = await context.Bookings
+                .Include(b => b.Slot)
+                .Include(b => b.Booker)
+                .Where(b => b.Slot.StartAt >= firstDayOfWeek && b.Slot.EndAt < firstDayOfWeek.AddDays(7))
+                .Select(b => b.ToBookingResponseDTO())
+                .ToListAsync();
+
+            var OrdersOfTheWeek = await context.Orders
+                .Where(o => o.UpdatedAt >= firstDayOfWeek && o.PaymentIntent != null)
+                .Select(o => o.ToOrderResponseForTeacherDTO())
+                .ToListAsync();
+
+            var newStudents = await context.Users
+                .Where(u => u.CreatedAt >= firstDayOfWeek)
+                .Select(u => u.ToUserResponseDTO(null))
+                .ToListAsync();
+
+            var trafficData = new Activities
+            {
+                NewStudents = newStudents,
+                BookingsOftheWeek = bookinsOftheWeek,
+                OrdersOfTheWeek = OrdersOfTheWeek
+            };
+
+            return Ok(new ResponseDTO<Activities>
+            {
+                Message = "Activités envoyées qvec succés",
+                Data = trafficData,
+                Status = 200
+            });
+
+        }
+    }
+}
---
+++ TerminalApi/Models/Activities/Activities.cs
@@ -0,0 +1,9 @@
+﻿namespace TerminalApi.Models
+{
+    public class Activities
+    {
+        public List<OrderResponseForTeacherDTO> OrdersOfTheWeek { get; set; } = new List<OrderResponseForTeacherDTO>();
+        public List<BookingResponseDTO> BookingsOftheWeek { get; set; } = new List<BookingResponseDTO>();    
+        public List<UserResponseDTO> NewStudents { get; set; } = new List<UserResponseDTO>();
+    }
+}
